05a9d484699b17b4e67ce910135077d3866ada03
zini9188 <zqwz@naver.com>
2023-10-30 15:43:53 +0900

    test : 위도 경도 테스트 추가
    두 좌표의 거리 찾는 메서드
    중심 좌표에서 NKM의 둘레를 갖는 사각형 찾는 메서드

---
+++ src/test/java/com/tgd/trip/global/util/CoordinateTest.java
@@ -0,0 +1,39 @@
+package com.tgd.trip.global.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CoordinateTest {
+
+    @Test
+    public void 두_좌표의_거리를_찾는_테스트() {
+        // given
+        Coordinate first = new Coordinate(37.529704, 126.957173);
+        Coordinate second = new Coordinate(37.523995, 127.025066);
+
+        // when
+        double degree = Coordinate.getHaversineDistance(first, second);
+
+        // then
+        assertThat(degree).isGreaterThan(6);
+    }
+
+    @Test
+    public void 중심_좌표에서_N_KM의_좌표를_찾는_테스트() {
+        // given
+        double height = 3.0;
+        double width = 3.0;
+        Coordinate center = new Coordinate(37.551088, 126.991929);
+
+        // when
+        Pair<Coordinate> squareCoordinate = center.getSquareCoordinate(center.latitude(), center.longitude(), height, width);
+        Coordinate first = squareCoordinate.first();
+        Coordinate second = squareCoordinate.second();
+
+        // then
+        // 한 변의 길이가 6인 정사각형의 대각선 길이는 11.04
+        System.out.println(Coordinate.getHaversineDistance(first, second));
+        assertThat(Coordinate.getHaversineDistance(first, second)).isGreaterThan(11);
+    }
+}
