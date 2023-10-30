b04c2fc1143fbbc863bce047f78c6d0682c7a8a7
zini9188 <zqwz@naver.com>
2023-10-30 15:42:42 +0900

    feat : 위도 경도 클래스 생성

---
+++ src/main/java/com/tgd/trip/global/util/Coordinate.java
@@ -0,0 +1,34 @@
+package com.tgd.trip.global.util;
+
+public record Coordinate(double latitude, double longitude) {
+
+    private static final double EARTH_RADIUS = 6371;
+
+    public static double getHaversineDistance(Coordinate first, Coordinate second) {
+        double lat = Math.toRadians(first.latitude() - second.latitude());
+        double lng = Math.toRadians(first.longitude() - second.longitude());
+
+        double dist = Math.sin(lat / 2) * Math.sin(lat / 2)
+                + Math.cos(Math.toRadians(first.latitude())) * Math.cos(Math.toRadians(second.latitude()))
+                * Math.sin(lng / 2) * Math.sin(lng / 2);
+
+        return 2 * Math.atan2(Math.sqrt(dist), Math.sqrt(1 - dist)) * EARTH_RADIUS;
+    }
+
+    public Pair<Coordinate> getSquareCoordinate(double centerX, double centerY, double height, double width) {
+        // 경도 변화량 계산 (단위: 도)
+        double longitudeDelta = Math.toDegrees(width / EARTH_RADIUS / Math.cos(Math.toRadians(centerY)));
+
+        // 위도 변화량 계산 (단위: 도)
+        double latitudeDelta = Math.toDegrees(height / EARTH_RADIUS);
+        Delta delta = new Delta(longitudeDelta, latitudeDelta);
+
+        return new Pair<>(
+                new Coordinate(centerX - delta.longitudeDelta(), centerY - delta.latitudeDelta()),
+                new Coordinate(centerX + delta.longitudeDelta(), centerY + delta.latitudeDelta())
+        );
+    }
+
+    private record Delta(double longitudeDelta, double latitudeDelta) {
+    }
+}
