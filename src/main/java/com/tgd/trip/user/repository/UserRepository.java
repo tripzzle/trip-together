9249b26de120fa4f25cfe5c38a9816714c7b235f
zini9188 <zqwz@naver.com>
2023-10-31 13:38:37 +0900

    feat : userRepository 생성

---
+++ src/main/java/com/tgd/trip/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tgd.trip.user.repository;
+
+import com.tgd.trip.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
