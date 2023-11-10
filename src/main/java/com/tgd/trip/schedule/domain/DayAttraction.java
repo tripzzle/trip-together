604eb2714ed2f3778f728c287cf5713103d69df0
zqwz <zqwz@naver.com>
2023-11-10 16:56:37 +0900

    refactor : 일정 생성 로직 수정

--- src/main/java/com/tgd/trip/schedule/domain/DayAttraction.java
+++ src/main/java/com/tgd/trip/schedule/domain/DayAttraction.java
@@ -5,6 +5,7 @@ import com.tgd.trip.global.BaseEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Optional;
 
 @Entity
 @Getter
@@ -15,13 +16,19 @@ public class DayAttraction extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long dayAttractionId;
-    @OneToOne(fetch =  FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "attraction_id")
     private Attraction attraction;
-    @ManyToOne(cascade = CascadeType.ALL, fetch =  FetchType.LAZY)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "day_id")
     private Day day;
     @Column(columnDefinition = "TEXT")
     private String memo;
     private Integer sequence;
+
+    public DayAttraction(Attraction attraction, String memo) {
+        this.attraction = attraction;
+        Optional.of(memo)
+                .ifPresent(this::setMemo);
+    }
 }
