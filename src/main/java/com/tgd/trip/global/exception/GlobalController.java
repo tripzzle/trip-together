10bacc505009a9911f9ce8766c665a896d41914e
zini9188 <zqwz@naver.com>
2023-11-15 08:57:34 +0900

    feat : 예외 핸들링 추가

--- src/main/java/com/tgd/trip/global/exception/GlobalController.java
+++ src/main/java/com/tgd/trip/global/exception/GlobalController.java
@@ -1,8 +1,14 @@
 package com.tgd.trip.global.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalController {
 
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getHttpStatus(), e.getErrorCode().getMessage());
+        return ResponseEntity.status(errorResponse.status()).body(errorResponse);
+    }
 }
