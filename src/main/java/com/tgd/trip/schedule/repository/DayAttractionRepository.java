389884fd3bcc8ed5a9cacbaba60823be36f8176e
zini9188 <zqwz@naver.com>
2023-11-14 14:18:35 +0900

    feat : 일자 관광지 정보 일괄 삭제 기능 추가

--- src/main/java/com/tgd/trip/schedule/repository/DayAttractionRepository.java
+++ src/main/java/com/tgd/trip/schedule/repository/DayAttractionRepository.java
@@ -1,7 +1,14 @@
 package com.tgd.trip.schedule.repository;
 
 import com.tgd.trip.schedule.domain.DayAttraction;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DayAttractionRepository extends JpaRepository<DayAttraction, Long> {
+
+    @Modifying
+    @Query("delete from DayAttraction da where da.dayAttractionId in :ids")
+    void deleteAllByIds(@Param("ids") List<Long> ids);
 }
