033d53350980904beff406582d4d1716bb72e0dc
wjstmdduf <wjs6265@gmail.com>
2023-11-15 16:28:33 +0900

    Fix : 열거형 -> List<String> 수정

--- src/main/java/com/tgd/trip/user/domain/User.java
+++ src/main/java/com/tgd/trip/user/domain/User.java
@@ -2,11 +2,15 @@ package com.tgd.trip.user.domain;
 
 import com.tgd.trip.global.BaseEntity;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -24,26 +28,36 @@ public class User extends BaseEntity {
     @Column(unique = true)
     private String email;
     @NotNull
-    private Role role;
+    @ElementCollection
+    private List<String> roles = new ArrayList<String>();
     private String provider;
     private String providerId;
-    @Null
     private UserStatus status=UserStatus.ACTIVE;
     private LocalDate birth;
     private String nickName;
     private Boolean sex;
 
     @Builder
-    public User(Long userId, String password, String name, String email, Role role, String provider, String providerId, LocalDate birth, String nickName,  Boolean sex){
+    public User(Long userId, String password, String name, String email, List<String> roles, String provider, String providerId, LocalDate birth, String nickName,  Boolean sex){
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
-        this.role = role;
+        this.roles = roles;
         this.provider = provider;
         this.providerId = providerId;
         this.birth = birth;
         this.nickName = nickName;
         this.sex = sex;
+    }
+
+    @Builder
+    public User( String password, String name, String email, List<String> roles, String provider, String providerId){
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.roles = roles;
+        this.provider = provider;
+        this.providerId = providerId;
     }
 }
