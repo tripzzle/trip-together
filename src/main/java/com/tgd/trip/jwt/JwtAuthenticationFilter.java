fc8d795ceb2d3bfd183e369f61bb7f6f0f2e5183
wjstmdduf <wjs6265@gmail.com>
2023-11-15 16:26:08 +0900

    Add : jwt 토큰 설정 추가

--- src/main/java/com/tgd/trip/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/tgd/trip/jwt/JwtAuthenticationFilter.java
@@ -20,28 +20,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+public class JwtAuthenticationFilter extends GenericFilterBean {
 
-    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
-            return authenticationManager.authenticate(authenticationToken);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 헤더에서 JWT를 받아옴
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        // 유효한 토큰인지 확인
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옴
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            // SecurityContext에 Authentication 객체를 저장
+//            System.out.println(authentication.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+//            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+
         }
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain filterChain,
-                                            Authentication authentication) throws IOException, ServletException {
-        this.getSuccessHandler().onAuthenticationSuccess(request, response, authentication);
+        chain.doFilter(request, response);
     }
 }
