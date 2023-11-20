42441e10a4469702cb094abc162fcef7899b1237
wjstmdduf <wjs6265@gmail.com>
2023-11-20 14:09:50 +0900

    fix : Role 컬럼 수정

--- src/main/java/com/tgd/trip/user/domain/User.java
+++ src/main/java/com/tgd/trip/user/domain/User.java
@@ -28,10 +28,11 @@ public class User extends BaseEntity {
     private String email;
     @NotNull
     @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "USER_ROLES",    joinColumns = @JoinColumn(name = "USER_ID"))
     private List<String> roles = new ArrayList<>();
     private String provider;
     private String providerId;
-    private UserStatus status = UserStatus.ACTIVE;
+    private UserStatus status=UserStatus.ACTIVE;
     private LocalDate birth;
     private String nickName;
     private String imgUrl;
@@ -42,7 +43,7 @@ public class User extends BaseEntity {
     private Boolean sex;
 
     @Builder
-    public User(Long userId, String password, String name, String email, List<String> roles, String provider, String providerId, LocalDate birth, String nickName, Boolean sex) {
+    public User(Long userId, String password, String name, String email, List<String> roles, String provider, String providerId, LocalDate birth, String nickName,  Boolean sex){
         this.userId = userId;
         this.password = password;
         this.name = name;
@@ -56,7 +57,7 @@ public class User extends BaseEntity {
     }
 
     @Builder
-    public User(String password, String name, String email, List<String> roles, String provider, String providerId) {
+    public User( String password, String name, String email, List<String> roles, String provider, String providerId){
         this.password = password;
         this.name = name;
         this.email = email;
