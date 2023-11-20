f68116ae58da9917f8a9f3eb3dbe5ddeee996a06
wjstmdduf <wjs6265@gmail.com>
2023-11-20 20:13:37 +0900

    add : 유저 프로필 사진 업로드

--- src/main/java/com/tgd/trip/user/service/UserService.java
+++ src/main/java/com/tgd/trip/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.tgd.trip.user.service;
 
+import com.tgd.trip.global.s3.S3Uploader;
 import com.tgd.trip.jwt.JwtTokenProvider;
 import com.tgd.trip.user.domain.Role;
 import com.tgd.trip.user.domain.User;
@@ -7,6 +8,7 @@ import com.tgd.trip.user.dto.SignupDto;
 import com.tgd.trip.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +19,9 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
     private final JwtTokenProvider provider;
+
     public User getVerifyUser(String email) {
         return userRepository.findByEmail(email).orElseThrow();
     }
@@ -47,8 +51,9 @@ public class UserService {
         return user;
     }
 
-    public String postSignup(SignupDto user) {
+    public String postSignup(SignupDto user, MultipartFile file) {
         String newToken = null;
+        String imgUrl = "";
         //signupDto로 추가정보 안한 유저 정보 가져옴
         Optional<User> optionalUser = userRepository.findById(user.getUserId());
         if (optionalUser.isPresent()) {
@@ -58,6 +63,11 @@ public class UserService {
             oldUser.setSex(user.getSex());
 
             oldUser.getRoles().add(Role.USER.name());
+            if(file != null){
+                imgUrl = s3Uploader.saveUploadFile(file);
+                imgUrl = s3Uploader.getFilePath(imgUrl);
+            }
+            oldUser.setImgUrl(imgUrl);
             System.out.println(oldUser);
 
             userRepository.save(oldUser);
