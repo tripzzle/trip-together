896605fec5a752c6ffecb8dabce0ccf77aa7342f
wjstmdduf <wjs6265@gmail.com>
2023-11-20 14:07:52 +0900

    fix :  email인자값 받는거 userId로 변경

--- src/main/java/com/tgd/trip/user/service/CustomOAuth2User.java
+++ src/main/java/com/tgd/trip/user/service/CustomOAuth2User.java
@@ -1,13 +1,10 @@
 package com.tgd.trip.user.service;
 
-import com.tgd.trip.user.domain.Role;
 import com.tgd.trip.user.domain.User;
 import com.tgd.trip.util.AuthorityUtils;
 import lombok.Getter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
@@ -16,17 +13,17 @@ import java.util.Map;
 @Getter
 public class CustomOAuth2User extends DefaultOAuth2User {
 
-    private final String email;
     private final List<String> roles;
+    private final Long userId;
 
     public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
                             Map<String, Object> attributes,
                             String nameAttributeKey,
-                            String email,
+                            Long userId,
                             List<String> roles) {
         super(authorities, attributes, nameAttributeKey);
-        this.email = email;
         this.roles = roles;
+        this.userId = userId;
     }
 
     public static CustomOAuth2User of(User user, Map<String, Object> attributes, OAuthAttributes oAuthAttributes) {
@@ -34,7 +31,8 @@ public class CustomOAuth2User extends DefaultOAuth2User {
                 AuthorityUtils.getAuthorities(user.getRoles()),
                 attributes,
                 oAuthAttributes.getProviderId(),
-                user.getEmail(),
-                user.getRoles());
+                user.getUserId(),
+                user.getRoles()
+        );
     }
 }
