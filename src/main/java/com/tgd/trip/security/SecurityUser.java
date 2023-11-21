8daeb1706d344651fc2e00107100784fdbb394c0
wjstmdduf <wjs6265@gmail.com>
2023-11-21 18:00:04 +0900

    refactor : 룰 전체 받아오게 수정

--- src/main/java/com/tgd/trip/security/SecurityUser.java
+++ src/main/java/com/tgd/trip/security/SecurityUser.java
@@ -1,14 +1,20 @@
 package com.tgd.trip.security;
 
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+
+import java.util.stream.Collectors;
 
 public class SecurityUser extends User {
     private com.tgd.trip.user.domain.User user;
 
     public SecurityUser(com.tgd.trip.user.domain.User user) {
         super(user.getEmail(), user.getPassword(),
-                AuthorityUtils.createAuthorityList("ROLE_" + user.getRoles().stream().findFirst().get()));
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .collect(Collectors.toList()));
+
         this.user = user;
     }
 
