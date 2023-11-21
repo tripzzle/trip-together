2ca9529d2310e2b05d52a496aa92eb4c02dc76db
wjstmdduf <wjs6265@gmail.com>
2023-11-21 17:59:47 +0900

    refactor : 필요없는 코드 수정

--- src/main/java/com/tgd/trip/security/SecurityConfig.java
+++ src/main/java/com/tgd/trip/security/SecurityConfig.java
@@ -1,14 +1,14 @@
 package com.tgd.trip.security;
 
-import com.tgd.trip.jwt.*;
+import com.tgd.trip.jwt.JwtAuthenticationFilter;
+import com.tgd.trip.jwt.JwtTokenProvider;
+import com.tgd.trip.jwt.OAuth2LoginFailureHandler;
+import com.tgd.trip.jwt.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
